93a65b17b4ad874dd0502d37e2e4d8dd597deda3
senya_1997work <senya_1997@mail.ru>
2020-05-29 16:01:28 +0300

    add ROM block with .mif

---
+++ fht_rom_block.v
@@ -0,0 +1,38 @@
+module fht_rom_block #(parameter W_BIT = 12, A_BIT = 8)(
+	input iCLK,
+	input iRESET,
+	
+	input [A_BIT - 1 : 0] iADDR,
+	
+	output signed [W_BIT - 1 : 0] oSIN_0,
+	output signed [W_BIT - 1 : 0] oCOS_0,
+	
+	output signed [W_BIT - 1 : 0] oSIN_1,
+	output signed [W_BIT - 1 : 0] oCOS_1
+);
+
+wire signed [W_BIT - 1 : 0] OUT_SIN;
+wire signed [W_BIT - 1 : 0] OUT_COS;
+
+fht_rom #(.MIF("./matlab/sin.mif")) ROM_SIN(
+	.address(iADDR),
+	.clock(iCLK),
+	.q(OUT_SIN)
+);
+
+fht_rom #(.MIF("./matlab/cos.mif")) ROM_COS(
+	.address(iADDR),
+	.clock(iCLK),
+	.q(OUT_COS)
+);
+
+// cos(x) = sin(y)
+// cos(y) = -sin(x)
+
+assign oSIN_0 = OUT_SIN;
+assign oCOS_0 = OUT_COS;
+
+assign oSIN_1 = OUT_COS;
+assign oCOS_1 = {~OUT_SIN[W_BIT - 1], OUT_SIN[W_BIT - 2 : 0]};
+
+endmodule 
--- matlab/get_mif.m
+++ matlab/get_mif.m
@@ -1,26 +1,18 @@
 clear;
 clc;
 
-%    in '.mif' complex coef save in one data where   %
-%    1st half MSB is imag part of coef               %
-%    2ns LSB is real part, e.g. for 12 bit coef:     %
-%                imag(1) | real(2)                   %
-%                23...12 | 11...0                    %
-
-width = 12; % imag and real separate
+width = 12;
 depth = 1024;
 
 fprintf('\n\tBegin\n');
 
-%% read files:
-    w_re(1:depth, 1:3) = [load('w_re_1.txt'), load('w_re_2.txt'), load('w_re_3.txt')];
-    w_im(1:depth, 1:3) = [load('w_im_1.txt'), load('w_im_2.txt'), load('w_im_3.txt')];
-    
-    file_mif(1:3) = [fopen('rom_1.mif', 'wt'), fopen('rom_2.mif', 'wt'), fopen('rom_3.mif', 'wt')];
+% read files:
+    coef(1:depth, 1:2) = [load('sin.txt'), load('cos.txt')];
+    file_mif(1:2) = [fopen('sin.mif', 'wt'), fopen('cos.mif', 'wt')];
 
-%% descriptor:
-for i = 1:3
-	fprintf(file_mif(i), 'WIDTH=%d;\n', 2*width);
+% descriptor:
+for i = 1:2
+	fprintf(file_mif(i), 'WIDTH=%d;\n', width);
 	fprintf(file_mif(i), 'DEPTH=%d;\n', depth);
 	fprintf(file_mif(i), '\n');
 	fprintf(file_mif(i), 'ADDRESS_RADIX=UNS;\n');
@@ -31,19 +23,15 @@ end
 
 fprintf('\n\t\tadd data in ".mif"...\n');
 
-%% add data:
-for k = 1:3
+% add data:
+for k = 1:2
     for i = 1:depth
-        w_str = [num2str(dec2hex((w_im(i, k) < 0)*2^width + w_im(i, k), width/4)),...
-                 num2str(dec2hex((w_re(i, k) < 0)*2^width + w_re(i, k), width/4))];
-
-        fprintf(file_mif(k), '%d\t:\t%s;\n', i - 1, w_str);
+        str = num2str(dec2hex((coef(i, k) < 0)*2^width + coef(i, k), width/4));
+        fprintf(file_mif(k), '%d\t:\t%s;\n', i - 1, str);
     end
-end
-
-for i = 1:3
-    fprintf(file_mif(i),'END;');
-    fclose(file_mif(i));
+    
+    fprintf(file_mif(k),'END;');
+    fclose(file_mif(k));
 end
 
 fprintf('\n\tComplete\n');
