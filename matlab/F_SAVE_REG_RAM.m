3f25f0f7cefbd494d40ab5169a1722e5573d6a87
shepelsky_ag <senya_1997@mail.ru>
2021-11-24 15:13:32 +0300

    fix save reg ver of ram from matlab

--- matlab/F_SAVE_REG_RAM.m
+++ matlab/F_SAVE_REG_RAM.m
@@ -1,15 +1,13 @@
 function F_SAVE_REG_RAM = F_SAVE_REG_RAM(ram, bit, file_name)
 
+N_bank = 4;
+
 % save fixed point version of RAM for conv:
 row = length(ram);
 abs_max_ram = 0;
 
 for i = 1 : row
-    if(max(ram(i,:)) > abs(min(ram(i,:))))
-        cur_max_ram = max(ram(i,:));
-    else
-        cur_max_ram = abs(min(ram(i,:)));
-    end
+    cur_max_ram = max(abs(ram(i,:)));
 
     if(cur_max_ram > abs_max_ram)
         abs_max_ram = cur_max_ram;
@@ -21,7 +19,21 @@ if(file_reg == -1)
 	error('Error: file name is wrong "%s"', file_name); 
 end
 
-reg_ram = round(ram*(2^(bit - 1))/abs_max_ram); % fixed point for FPGA like registers
+norm_coef_p = (2^(bit - 1) - 1)/abs_max_ram;
+norm_coef_n = (2^(bit - 1))/abs_max_ram;
+
+% fixed point for FPGA like registers:
+reg_ram(1:row, 1:N_bank) = zeros;
+
+for i = 1:N_bank
+    for j = 1:row
+        if(ram(j, i) < 0)
+            reg_ram(j, i) = round(ram(j, i)*norm_coef_n);
+        else
+            reg_ram(j, i) = round(ram(j, i)*norm_coef_p);
+        end
+    end
+end
 
 for i = 1 : row
     fprintf(file_reg, '%6d\t%6d\t%6d\t%6d\n', reg_ram(i, :));
